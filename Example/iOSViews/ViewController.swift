74d11263a4f818e6dbb9a6eded523de63b9b82f1
winfred_zen <869158130@qq.com>
2018-07-22 10:24:13 +0800

    Initial commit

---
+++ Example/iOSViews/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSViews
+//
+//  Created by winfredzen on 07/22/2018.
+//  Copyright (c) 2018 winfredzen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
